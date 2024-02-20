6293bc4dab8783b73905bf8d50c8ee77612dbcba
jerry-li65 <jerry.li65@tamu.edu>
2024-02-20 14:08:32 -0600

    TC-29 init connecting firebase. Android/iOS. Barebones.

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'second_page.dart';
 import 'third_page.dart';
 import 'fourth_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
+
+// void main() {
+//   runApp(MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   @override

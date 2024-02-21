47398120ac5a47e555299e921acefc25dd92e353
Tommy4367 <tommygarriga4367@gmail.com>
2024-02-21 10:07:46 -0600

    Firebase initialized

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -6,7 +6,7 @@ import 'fourth_page.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
 
   runApp(MyApp());
 }

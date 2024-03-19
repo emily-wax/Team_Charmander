dca0a98879b18b4ee8dd02d039b92cf2d56775ae
emily-wax <erenee2020@gmail.com>
2024-03-19 13:32:53 -0500

    bug fixes from main

--- flutter_application/lib/SignInPage.dart
+++ flutter_application/lib/SignInPage.dart
@@ -73,7 +73,7 @@ class _SignInPageState extends State<SignInPage> {
               ElevatedButton(
                 onPressed: isLoading ? null : () => _authenticate(),
                 style: ElevatedButton.styleFrom(
-                  primary: Colors.white, // White button
+                  backgroundColor: Colors.white, // White button
                 ),
                 child: Text(isSignUp ? 'Sign Up' : 'Sign In'),
               ),
@@ -85,7 +85,7 @@ class _SignInPageState extends State<SignInPage> {
                   });
                 },
                 style: TextButton.styleFrom(
-                  primary: Colors.blue, // Blue color for "already have an account?"
+                  backgroundColor: Colors.blue, // Blue color for "already have an account?"
                 ),
                 child: Text(
                   isSignUp

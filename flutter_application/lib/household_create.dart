9ae755d31f0bf103525aebc51abdb138c8548846
emily-wax <erenee2020@gmail.com>
2024-03-23 21:46:27 -0500

    household creation form done

--- flutter_application/lib/household_create.dart
+++ flutter_application/lib/household_create.dart
@@ -7,15 +7,92 @@ class HouseholdCreate extends StatelessWidget{
   @override
 
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Household Creation Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HouseholdCreateForm(),
+    );
+
+  }
+}
+
+class HouseholdCreateForm extends StatefulWidget {
+  @override
+  _HouseholdCreateFormState createState() => _HouseholdCreateFormState();
+}
+
+
+class _HouseholdCreateFormState extends State<HouseholdCreateForm> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _countController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Create Household'),
+        title: Text('Household Creation Form'),
       ),
-
-      // body: Container(
-
-      // )
-
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Household Name',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter the name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _countController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Maximum Roommate Count',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter the count';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Process the data
+                      String name = _nameController.text;
+                      int count = int.parse(_countController.text);
+                      /* TODO: add to database here */
+                      print('Name: $name, Count: $count');
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _countController.dispose();
+    super.dispose();
   }
 }

f8c98a03eaae09e574299cf590ec776ff04bea20
jerry-li65 <jerry.li65@tamu.edu>
2024-03-27 12:48:10 -0500

    preferences save as hashmap to incorrect database, as expected :)

--- flutter_application/lib/fourth_page.dart
+++ flutter_application/lib/fourth_page.dart
@@ -6,16 +6,14 @@ import 'household_join.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 // TODO: add a password for joining the house
-// TODO: household auto-deletes when no members are in? 
+// TODO: household auto-deletes when no members are in?
 
 class FourthPage extends StatefulWidget {
   @override
   _FourthPageState createState() => _FourthPageState();
 }
 
-
 class _FourthPageState extends State<FourthPage> {
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
   List<HouseholdModel> _households = [];
   bool _showJoinButton = true; // boolean to control visibility of Join button
@@ -34,22 +32,26 @@ class _FourthPageState extends State<FourthPage> {
           .where('roommates', arrayContains: currentUser.email)
           .get();
       setState(() {
-        _households = snapshot.docs.map((doc) => HouseholdModel.fromSnapshot(doc)).toList();
+        _households = snapshot.docs
+            .map((doc) => HouseholdModel.fromSnapshot(doc))
+            .toList();
         _showJoinButton = _households.isEmpty;
       });
     }
   }
 
-void removeFromHousehold(String houseName) {
-  User? _currentUser = _auth.currentUser;
+  void removeFromHousehold(String houseName) {
+    User? _currentUser = _auth.currentUser;
 
-  FirebaseFirestore.instance.collection('households')
-    .where('name', isEqualTo: houseName)
-    .get()
-    .then((querySnapshot) {
+    FirebaseFirestore.instance
+        .collection('households')
+        .where('name', isEqualTo: houseName)
+        .get()
+        .then((querySnapshot) {
       if (querySnapshot.docs.isNotEmpty) {
         var document = querySnapshot.docs.first;
-        List<dynamic> existingRoommates = List.from(document.data()['roommates']);
+        List<dynamic> existingRoommates =
+            List.from(document.data()['roommates']);
         if (existingRoommates.contains(_currentUser!.email)) {
           existingRoommates.remove(_currentUser.email);
           document.reference.update({'roommates': existingRoommates}).then((_) {
@@ -79,107 +81,158 @@ void removeFromHousehold(String houseName) {
     }).catchError((error) {
       print('Error retrieving household: $error');
     });
-}
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Account Page'),
       ),
-
       body: Container(
         color: Colors.orange, // Set the background color to orange
         child: Center(
-          child: Column(
-            mainAxisAlignment:  MainAxisAlignment.center,
-            children: <Widget>[ 
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
               FutureBuilder(
                 future: _readData(),
-                builder:( (context, snapshot) {
-                  if( snapshot.connectionState == ConnectionState.done){
-                    if(snapshot.hasData){
+                builder: ((context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
                       UserModel? user = snapshot.data as UserModel;
 
-                      // TODO: find a better way to display user data ... 
+                      // TODO: find a better way to display user data ...
 
                       // Display current user email and households
                       return Column(
                         children: [
                           Text(user.email!),
                           SizedBox(height: 20),
+                          // TODO: preferences here
+                          ElevatedButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text('Make Your Choices'),
+                                    content: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            _buildChoiceButton(context, 'Cleaner', 'tidy', 'cleaner'),
+                                            _buildChoiceButton(context, 'Organizer', 'tidy', 'organizer'),
+                                          ],
+                                        ),
+                                        SizedBox(height: 16),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            _buildChoiceButton(context, 'Early Riser', 'timeOfDay', 'earlyRiser'),
+                                            _buildChoiceButton(context, 'Night Owl', 'timeOfDay', 'nightOwl'),
+                                          ],
+                                        ),
+                                        SizedBox(height: 16),
+                                        // _buildChoiceButton(context, 'Cleaner'),
+                                        // _buildChoiceButton(context, 'Organizer'),
+                                        // _buildChoiceButton(context, 'Early Riser'),
+                                        // _buildChoiceButton(context, 'Night Owl'),
+                                        // SizedBox(height: 16),
+                                        ElevatedButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text('Submit'),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                            child: Text('Set Preferences'),
+                          ),
+
                           Text('User Household:'),
-                          SizedBox(height: 10,),
+                          SizedBox(
+                            height: 10,
+                          ),
                           ListView.builder(
                               shrinkWrap: true,
                               itemCount: _households.length,
-                              itemBuilder: (context, index){
+                              itemBuilder: (context, index) {
                                 return ListTile(
-                                  title: Text(_households[index].name),
-                                  subtitle: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: _households[index].roommates.map((email) => Text(email)).toList(),
-                                  ),
-                                  trailing: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text("Number of Household Members: ${_households[index].roommates.length}"),
-                                      SizedBox(width: 10),
-                                      IconButton(
-                                        onPressed: () {
-                                          removeFromHousehold(_households[index].name);
-                                        } , 
-                                        icon: Icon(Icons.delete),
-                                      )
-
-                                    ],
-                                    )
-                                    
-                                );
-                              }
-                            )
-                          
+                                    title: Text(_households[index].name),
+                                    subtitle: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: _households[index]
+                                          .roommates
+                                          .map((email) => Text(email))
+                                          .toList(),
+                                    ),
+                                    trailing: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Text(
+                                            "Number of Household Members: ${_households[index].roommates.length}"),
+                                        SizedBox(width: 10),
+                                        IconButton(
+                                          onPressed: () {
+                                            removeFromHousehold(
+                                                _households[index].name);
+                                          },
+                                          icon: Icon(Icons.delete),
+                                        )
+                                      ],
+                                    ));
+                              })
                         ],
                       );
                     }
-                  } 
-      
+                  }
+
                   return Center(child: CircularProgressIndicator());
                 }),
               ),
               SizedBox(height: 20),
               Visibility(
                 visible: _showJoinButton,
-                child: 
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => HouseholdCreate()),);
-                    }, 
-                    child: Text(
-                      'Create a Household',
-                      style: TextStyle(fontSize: 20),
-                    ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HouseholdCreate()),
+                    );
+                  },
+                  child: Text(
+                    'Create a Household',
+                    style: TextStyle(fontSize: 20),
                   ),
+                ),
               ),
-
               SizedBox(height: 20),
               Visibility(
                 visible: _showJoinButton,
-                child:
-                ElevatedButton(
+                child: ElevatedButton(
                   onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => HouseholdJoin()),);
-                  }, 
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HouseholdJoin()),
+                    );
+                  },
                   child: Text(
                     'Join a Household',
                     style: TextStyle(fontSize: 20),
                   ),
                 ),
               )
-            ]
-          )
-        ),
+            ])),
       ),
     );
   }
@@ -193,25 +246,56 @@ Future<UserModel> _readData() async {
 
   String? currEmail = user!.email;
 
-  final snapshot = await db.collection("users").where("email", isEqualTo: currEmail).get();
+  final snapshot =
+      await db.collection("users").where("email", isEqualTo: currEmail).get();
 
   final userData = snapshot.docs.map((e) => UserModel.fromSnapshot(e)).single;
-  
-  return userData;
 
+  return userData;
 }
 
+Widget _buildChoiceButton(BuildContext context, String choice, String category, String value) {
+  return ElevatedButton(
+    onPressed: () {
+      _submitChoice(category, value);
+    },
+    child: Text(choice),
+  );
+}
 
-class HouseholdModel{
+  void _submitChoice(String category, String value) {
+    FirebaseFirestore.instance.collection('users').doc('cxkVM8ZzLo9JzUWoS41B').get().then((DocumentSnapshot snapshot) {
+      Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
+      Map<String, String> preferences = data['preferences'] != null ? Map<String, String>.from(data['preferences']) : {};
+
+      preferences[category] = value;
+
+      FirebaseFirestore.instance.collection('users').doc('cxkVM8ZzLo9JzUWoS41B').set({
+        'preferences': preferences,
+      }, SetOptions(merge: true)).then((_) {
+        print('Preferences updated successfully!');
+      }).catchError((error) {
+        print('Failed to update preferences: $error');
+      });
+    }).catchError((error) {
+      print('Error getting document: $error');
+    });
+  }
+
+class HouseholdModel {
   final String name;
   final int max_roommate_count;
   final List<String> roommates;
 
-  HouseholdModel({required this.name, required this.max_roommate_count, required this.roommates});
+  HouseholdModel(
+      {required this.name,
+      required this.max_roommate_count,
+      required this.roommates});
 
   factory HouseholdModel.fromSnapshot(DocumentSnapshot snapshot) {
     Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
-    List<dynamic> roommates = data['roommates'] != null ? List.from(data['roommates']) : [];
+    List<dynamic> roommates =
+        data['roommates'] != null ? List.from(data['roommates']) : [];
     return HouseholdModel(
       name: data['name'],
       max_roommate_count: data['max_roommate_count'],

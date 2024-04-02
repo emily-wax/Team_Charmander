d36f2c91519878ce05e35ed8cf56dc5ffcf2fc46
emily-wax <erenee2020@gmail.com>
2024-04-02 08:51:14 -0500

    attempted to address line 20 comment

--- flutter_application/lib/auto_assign_chores.dart
+++ flutter_application/lib/auto_assign_chores.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_application/household_model.dart';
 import 'user_model.dart';
 import 'dart:math';
 
@@ -18,6 +19,10 @@ class AutoAssignClass extends StatefulWidget {
   // eventually, this function will be used to run the Modified Adjusted Winner Allocation Algorithm
   Future<String> _getUser() async {
     // TODO: Fetch and save all roommates' emails in the user's household
+    
+    UserModel currUserModel = await readData();
+    HouseholdModel currHouseModel = HouseholdModel.fromSnapshot(await FirebaseFirestore.instance.collection('households').doc(currUserModel.currHouse).get());
+    List<String> existingRoommates = currHouseModel.roommates; 
 
     // TODO: Fetch and save the "slider-prefs" in the following collection: pKyWYjznujaUilHDVHmM (this is the key of one of the roommates in household "Jerry Residence")
 

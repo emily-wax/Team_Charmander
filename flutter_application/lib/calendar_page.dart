1ebbce26c8cb1a768a3ada64fd1d54cdf9e86bad
emily-wax <erenee2020@gmail.com>
2024-04-11 15:46:53 -0500

    calendar page loads ... need date input

--- flutter_application/lib/calendar_page.dart
+++ flutter_application/lib/calendar_page.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // Import FirebaseAuth for user authentication
 import 'user_model.dart';
 import 'package:flutter_application/household_model.dart';
+import 'HomePage.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({Key? key}) : super(key: key);
@@ -25,23 +26,58 @@ class _CalendarPageState extends State<CalendarPage> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   String _householdId = ""; // Variable to store the household ID
+  UserModel? currUserModel;
 
   @override
   void initState() {
     super.initState();
     _calendarView = CalendarView.day;
     _updateDisplayDate();
-    _getHouseholdId(); // Fetch household ID when widget initializes
+   // _getHouseholdId(); // Fetch household ID when widget initializes
    
   }
 
-  @override
-  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shared Calendar'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+            },
+          )
+        ],
       ),
-      body: Column(
+
+      body: FutureBuilder<UserModel>(
+        future: readData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            currUserModel = snapshot.data; // Set currUserModel once future completes
+            _householdId = currUserModel!.currHouse!;
+            return buildCalendarPage(); // Build the main content of the page
+          }
+        },
+      ), 
+    );
+  }
+
+
+  Widget buildCalendarPage () {
+    return Column(
         children: [
           Expanded(
             child: StreamBuilder<QuerySnapshot>(
@@ -87,27 +123,27 @@ class _CalendarPageState extends State<CalendarPage> {
               child: const Text('Add Event'),
             ),
           ),
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: _handleAddEvent,
+          )
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: _handleAddEvent,
-      ),
-    );
+      );
+
   }
 
-  Future<void> _getHouseholdId() async {
-    UserModel currUserModel = await readData();
-    if(currUserModel.id != null){
-      HouseholdModel currHouseModel = HouseholdModel.fromSnapshot(
-          await FirebaseFirestore.instance
-              .collection('households')
-              .doc(currUserModel.currHouse)
-              .get());
+  // Future<void> _getHouseholdId() async {
+  //   UserModel currUserModel = await readData();
+  //   if(currUserModel.id != null){
+  //     HouseholdModel currHouseModel = HouseholdModel.fromSnapshot(
+  //         await FirebaseFirestore.instance
+  //             .collection('households')
+  //             .doc(currUserModel.currHouse)
+  //             .get());
 
-      _householdId = currHouseModel.name;
-    }
-  }
+  //     _householdId = currHouseModel.name;
+  //   }
+  // }
 
   void _handleAddEvent() {
     showDialog(
@@ -127,27 +163,29 @@ class _CalendarPageState extends State<CalendarPage> {
               ),
               const SizedBox(height: 16.0),
               // Time selection widgets...
+              // TODO: input time selection widgets
             ],
           ),
           actions: [
             // Cancel button...
             ElevatedButton(
               onPressed: () async {
-                if (eventName != null && _startTime != null && _endTime != null) {
+                /* add start and end time back */
+                if (eventName != null) {
                   final selectedDate = _calendarController.selectedDate;
                   if (selectedDate != null) {
                     UserModel currUserModel = await readData();
                     final event = {
                       'name': eventName,
-                      'start': _startTime!.format(context),
-                      'end': _endTime!.format(context),
+                      // 'start': _startTime!.format(context),
+                      // 'end': _endTime!.format(context),
                       'user': currUserModel.email, // Placeholder for user name, replace with actual user name
                     };
 
                     await _firestore
                         .collection('households')
                         .doc(_householdId) // Use the household ID obtained from Firestore
-                        .collection('Events')
+                        .collection('events')
                         .add(event);
                   }
                 }
